206d59e5e5849610702053514e0e05183493e5f4
berson969 <berson969@gmail.com>
2024-01-28 15:31:15 +0100

    README

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Tooltip from "./js/tooltip";
-import "./css/style.css";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const container = document.querySelector(".container");
-  const tooltip = new Tooltip(container);
-  tooltip.bindToDOM();
-});
